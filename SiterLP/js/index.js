4a20715c09905b478bdc2bc659ee253cada515c8
EvilBomj <erok_erok@mail.ru>
2019-09-13 02:01:10 +0600

    fix

--- SiterLP/js/index.js
+++ SiterLP/js/index.js
@@ -1,1 +1,19 @@
-import "./import/modules";
+$(document).ready(function(){
+    $(".select__active").click(function(e){
+        $active = this; // select
+        $select = $(this)
+        $select.toggleClass('cross'); // change icon
+        $('.select__active').not($select).removeClass('cross');
+        $element = $(this).parent().find(".select__list"); // optgroup
+        $('.select__list').not($element).removeClass('d-block'); // hidden opened optgroup
+        $element.toggleClass("d-block"); // show optgroup
+
+        $element.find('.select__tariff').click(function(e) {
+            $text = this.textContent; // option textContent
+            $active.textContent = $text; // select text === option text
+            $select.removeClass('cross'); // change icon
+            $element.removeClass('d-block'); // hidden optgroup
+            
+        })
+    });
+});
