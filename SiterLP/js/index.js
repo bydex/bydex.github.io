122be906d599e8d72e3ab34738ac5616fbc70058
xyembeu <knoppixglyuk@gmail.com>
2019-09-30 10:20:47 +0600

    fix

--- SiterLP/js/index.js
+++ SiterLP/js/index.js
@@ -64,16 +64,25 @@ $(document).ready(function(){
 
 
 
+    // browsers notifications 
+    var $buoop = { required: { e: -4, f: -3, o: -3, s: -1, c: -3 }, insecure: true, api: 2019.09 };
+    function $buo_f() {
+        var e = document.createElement("script");
+        e.src = "//browser-update.org/update.min.js";
+        document.body.appendChild(e);
+    };
+    try { document.addEventListener("DOMContentLoaded", $buo_f, false) }
+    catch (e) { window.attachEvent("onload", $buo_f) }
 
 
     function clickOut(cont, contClass, parent = cont) {
         $(document).mouseup(function (e) {
             var container = cont;
-            if (container.has(e.target).length === 0){
+            if (container.has(e.target).length === 0) {
                 parent.removeClass(contClass);
             }
         });
-    }
+    };
 
 
 
